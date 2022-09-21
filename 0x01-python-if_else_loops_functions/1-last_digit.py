4b51e1a34adc1e6e70bfd7ebc2df6eb2c063eeb6
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-09-21 15:00:04 -0700

    check for details

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,4 +8,6 @@ if(lastdig > 5):
 elif(lastdig == 0):
     print("Last digit of {0} is {1} and is zero".format(number, lastdig))
 elif(lastdig < 6 and not 0):
+    if(number < 0):
+        lastdig = -lastdig
     print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, lastdig))
