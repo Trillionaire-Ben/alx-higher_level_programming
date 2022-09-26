94f3dbe192ba698871d01318ec14b9c4d8092574
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-09-26 16:40:02 -0700

    2nd task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_a = (len(sentence), sentence[0] if sentence != () else None)
+    tup = (0, 'None')
+    tuple_a = (len(sentence), sentence[0] if len(sentence) != 0 else tup)
     return (tuple_a)
