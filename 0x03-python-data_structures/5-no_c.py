7c9c6724a792ad7a45e24a1bc44b2ad36f8bd6ec
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-09-25 18:50:11 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # Write a function that removes all characters c and C from a string.
-    for x in my_string:
-        i = "cC"
-        map_i = my_string.maketrans(x, x, i)
-        return (my_string.translate(map_i))
+    # Write a function that removes all characters c and C from a string
+    new_string = my_string.translate({ord(i): None for i in "cC"})
+    return (new_string)
