9f1eb91bc9b3aa94408f260295b657cc67c5a4bf
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-09-27 01:39:24 -0700

    fixed pycodestyle on task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-     my_tuple = ()
-     if len(sentence) == 0:
-         my_tuple = 0, "None"
-     else:
-         my_tuple = len(sentence), sentence[0]
-     return (my_tuple)
+    my_tuple = (len(sentence), sentence[0] if len(sentence) != 0 else "None")
+    return (my_tuple)
