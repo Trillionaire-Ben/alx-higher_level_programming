7b8f147aa828a3979722e3b22a287dbfbe5fb92b
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-10-12 02:37:20 -0700

    task 0 rewrite

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    ret_value = 0
-    while i in range(x):
+    i = 0
+    while (i < x):
         try:
             print("{}".format(my_list[i]), end="")
-            ret_value++
             i++
         except IndexError:
             break
     print("")
-    return (ret_value)
+    return (i)
