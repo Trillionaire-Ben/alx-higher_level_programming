21a16d416030333ef9eaede62c3ca64ac3aa15bf
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-10-24 03:43:46 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""A function that returns the list of available attributes and methods of an object"""
+
+def lookup(obj):
+    """should return the list of object attributes"""
+    return (dir(obj))
