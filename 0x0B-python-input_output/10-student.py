c23c757902b44533b0df0ad2c71fc0a68da1fca0
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-10-27 04:11:18 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student to JSON with filter """
+
+
+class Student:
+    """CLASS THAT DEFINES A STUDENT"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation
+
+        Args:
+            first_name (str): name
+            last_name (str): name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
