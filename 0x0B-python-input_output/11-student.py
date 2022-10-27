f8d2333ea2b5df0b939694b5dc83fda875e17040
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-10-27 04:16:46 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Student to disk and reload """
+
+
+class Student:
+    """CLASS THAT DEFINES A STUDENT"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation
+
+        Args:
+            first_name (str): name
+            last_name (str): name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
